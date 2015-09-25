6f1194a9c2558e02aa6d0d5aa9f05d477f70e06a
Harel Lustiger <harel@loostiger.com>
2015-09-25 17:55:20 +0300

    gender_based_model script and notebook are available

--- Python/scripts/reading_data.py
+++ Python/scripts/reading_data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-train = pd.read_csv('./data/train.csv', index_col='PassengerId')
-test = pd.read_csv('./data/test.csv', index_col='PassengerId')
+## Relative Path
+#train = pd.read_csv('./data/train.csv', index_col='PassengerId')
+#test = pd.read_csv('./data/test.csv', index_col='PassengerId')
 
+## Absolute Path
+train = pd.read_csv('C:/Repositories/Titanic/data/train.csv', index_col='PassengerId')
+test = pd.read_csv('C:/Repositories/Titanic/data/test.csv', index_col='PassengerId')
